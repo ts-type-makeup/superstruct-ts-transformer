c7017d74a8a8209a57624c56f08ead5283f24180
Pavel Shakhov <ab.pongo@gmail.com>
2019-08-16 12:15:17 +0300

    Typo

--- webpack-example/index.ts
+++ webpack-example/index.ts
@@ -1,6 +1,6 @@
 import { validate } from "superstruct-ts-transformer";
 
-type HttpBinGetResponce = {
+type HttpBinGetResponse = {
   args: { [key: string]: any };
   headers: {
     [key: string]: string;
@@ -11,7 +11,7 @@ type HttpBinGetResponce = {
 
 fetch("https://httpbin.org/get")
   .then(response => response.json())
-  .then(json => validate<HttpBinGetResponce>(json)) // <-- validate call!
+  .then(json => validate<HttpBinGetResponse>(json)) // <-- validate call!
   .then(httpBinResponse => {
     const responseStr = JSON.stringify(httpBinResponse, undefined, "  ");
     document.body.innerHTML = `<div style="color:black;">${responseStr}</div>`;
